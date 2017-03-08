8db73491886f2016b2a34160d7981ef722d4c47d
Patrick <pvdwesten@hotmail.com>
2017-03-08 14:00:23 +0100

    Horstmann/Secure Thermostat
    First Working version

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Horstmann
 
-This app adds support for Secure / Horstmann thermostat !
+This app adds support for Secure / Horstmann devices!
+
+Currently supported devices:
+* HRT4-ZW   http://products.z-wavealliance.org/products/125  Patrick (pvdwesten at hotmail.com)
+* SRT321  	http://products.z-wavealliance.org/products/115  Patrick (pvdwesten at hotmail.com)
+
+Changelog:
+Version 0.0.2
+	Working: 
+		- The boiler contol unit to work from both the Thermostat and homey (switching it on / off)
+		- The battery status from the Thermostat
+		- The actual temperature from the Thermostat
+
+	Not Working:
+		- Status changed of the boiler unit  (so not visible in Homey, when turned on/ off by thermostat
+		- Thermostat setpoint changed by homey
+		- Actual thermostat setpoint
+
+
+Version 0.0.1
+	 First version
+
--- drivers/SRT321/driver.js
+++ drivers/SRT321/driver.js
@@ -1,76 +1,78 @@
 'use strict';
 
 const path = require('path');
-const ZwaveDriver = require('node-homey-zwavedriver');
+const ZwaveDriver = require('homey-zwavedriver');
 
-// http://www.vesternet.com/downloads/dl/file/id/196/product/1128/z_wave_danfoss_lc_13_living_connect_radiator_thermostat_manual.pdf
+// http://www.pepper1.net/zwavedb/device/51
+// http://www.pepper1.net/zwavedb/device/170
+// http://products.z-wavealliance.org/products/115
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
-		measure_battery: {
-			getOnWakeUp: true,
-			command_class: 'COMMAND_CLASS_BATTERY',
-			command_get: 'BATTERY_GET',
-			command_report: 'BATTERY_REPORT',
-			command_report_parser: (report, node) => {
-
-				// If prev value is not empty and new value is empty
-				if (node && node.state && node.state.measure_battery !== 1 && report['Battery Level'] === "battery low warning") {
-
-					// Trigger device flow
-					Homey.manager('flow').triggerDevice('battery_alarm', {}, {}, node.device_data, err => {
-						if (err) console.error('Error triggerDevice -> battery_alarm', err);
-					});
-				}
-
-				if (report['Battery Level'] === "battery low warning") return 1;
+		'measure_battery': {
+			'getOnWakeUp': true,
+			'command_class'				: 'COMMAND_CLASS_BATTERY',
+			'command_get'				: 'BATTERY_GET',
+			'command_report'			: 'BATTERY_REPORT',
+			'command_report_parser'		: function( report ) {
 				return report['Battery Level (Raw)'][0];
 			}
-		},
-		target_temperature: {
-			command_class: 'COMMAND_CLASS_THERMOSTAT_SETPOINT',
-			command_get: 'THERMOSTAT_SETPOINT_GET',
-			command_get_parser: function () {
+		},		
+		'measure_temperature': {
+			'getOnWakeUp': true,
+			'command_class'				: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			'command_get'				: 'SENSOR_MULTILEVEL_GET',
+			'command_get_parser'		: function(){
 				return {
-					'Level': {
-						'Setpoint Type': 'Heating 1',
+					'Sensor Type': 'Temperature (version 1)',
+					'Properties1': {
+						'Scale': 0
 					}
-				};
-			},
-			command_set: 'THERMOSTAT_SETPOINT_SET',
-			command_set_parser: function (value, node) {
-
-				module.exports.realtime(node.device_data, 'target_temperature', Math.round(value * 2) / 2);
-
-				// Create value buffer
-				let a = new Buffer(2);
-				a.writeUInt16BE(( Math.round(value * 2) / 2 * 10).toFixed(0));
-
-				return {
-					'Level': {
-						'Setpoint Type': 'Heating 1'
-					},
-					'Level2': {
-						'Size': 2,
-						'Scale': 0,
-						'Precision': 1
-					},
-					'Value': a
-				};
-			},
-			command_report: 'THERMOSTAT_SETPOINT_REPORT',
-			command_report_parser: report => {
-				if (report.hasOwnProperty('Level2')
-					&& report.Level2.hasOwnProperty('Scale')
-					&& report.Level2.hasOwnProperty('Precision')
-					&& report.Level2['Scale'] === 0
-					&& report.Level2['Size'] !== 'undefined'
-					&& typeof report['Value'].readUIntBE(0, report.Level2['Size']) !== 'undefined') {
-					return report['Value'].readUIntBE(0, report.Level2['Size']) / Math.pow(10, report.Level2['Precision']);
 				}
-				return null;
 			},
+			'command_report'			: 'SENSOR_MULTILEVEL_REPORT',
+			'command_report_parser'		: function( report ){
+				if( report['Sensor Type'] !== 'Temperature (version 1)' )
+					return null;
+
+				return report['Sensor Value (Parsed)'];
+			}
 		},
+		'target_temperature': {
+			'getOnWakeUp': true,
+			'setOnWakeUp': true,
+			'command_class'				: 'COMMAND_CLASS_THERMOSTAT_SETPOINT',
+			'command_get'				: 'THERMOSTAT_SETPOINT_GET',
+			'command_get_parser'		: function(){
+				return {
+					'Sensor Type': 'Set Point (version 1)',
+					'Properties1': {
+						'Scale': 0
+					}
+				}
+			},
+			'command_set'				: 'THERMOSTAT_SETPOINT_SET',
+			'command_report'			: 'THERMOSTAT_SETPOINT_REPORT',
+			'command_report_parser'		: function( report ){
+				if( report['Sensor Type'] !== 'Set Point (version 1)' )
+					return null;
+
+				return report['Sensor Value (Parsed)'];
+			}
+				}
 	},
-	settings: {}
+	settings: {
+		"enable_temperature_sensor": {
+		"index": 1,
+		"size": 1
+		},
+		"temperature_unit": {
+			"index": 2,
+			"size": 1
+		},
+		"temperature_delta": {
+			"index": 3,
+			"size": 1
+		}
+	}
 });
