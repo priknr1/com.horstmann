2d72130cd1bba754dc6af0a7fd6291a5c9d7f57e
Patrick <pvdwesten@hotmail.com>
2017-04-06 08:43:40 +0200

    Set Target, Get target
    Target temperature can be changed. Also on local change, target
    temperature is updated.
    
    On local change, Target in Homey is updated immediatly,  On change in
    Homey, Target is updated after wke-up of the Thermostat

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
-# Horstmann
+# Horstmann/Secure
 
-This app adds support for Secure / Horstmann devices!
+This app adds support for Secure / Horstmann devices.
+If updating from V0.0.2 to V0.0.3  the units have to be re-added
+
 
 ##Currently supported devices:
 * HRT4-ZW   http://products.z-wavealliance.org/products/125  Patrick (pvdwesten at hotmail.com)
 * SRT321  	http://products.z-wavealliance.org/products/115  Patrick (pvdwesten at hotmail.com)
 
+
+##Not Working:
+* Status changed of the boiler unit  (so not visible in Homey, when turned on/ off by thermostat
+
+
 ##Changelog:
-* Version 0.0.2
-	Working: 
-	- The boiler contol unit to work from both the Thermostat and homey (switching it on / off)
-	- The battery status from the Thermostat
-	- The actual temperature from the Thermostat
-
-	Not Working:
-	- Status changed of the boiler unit  (so not visible in Homey, when turned on/ off by thermostat
-	- Thermostat setpoint changed by homey
-	- Actual thermostat setpoint
+ 
+0.0.3
+* Set target temperature on SRT321
+* Receive target temperature on local change SRT321
 
 
-*Version 0.0.1
-First version
+0.0.2
+* First Functionality for HRT4-ZW
+* First Functionality for SRT321
+* Battery and Actual temp status, 
+* turn on/off boiler unit 
 
+0.0.1
+* First version
--- drivers/SRT321/driver.js
+++ drivers/SRT321/driver.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const path = require('path');
+const path = require('path'); 
 const ZwaveDriver = require('homey-zwavedriver');
 
 // http://www.pepper1.net/zwavedb/device/51
@@ -8,21 +8,46 @@ const ZwaveDriver = require('homey-zwavedriver');
 // http://products.z-wavealliance.org/products/115
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
+	debug: true,
 	capabilities: {
-		'measure_battery': {
+		custom_thermostat_mode: {
 			'getOnWakeUp': true,
-			'command_class'				: 'COMMAND_CLASS_BATTERY',
-			'command_get'				: 'BATTERY_GET',
-			'command_report'			: 'BATTERY_REPORT',
-			'command_report_parser'		: function( report ) {
+			command_class: 'COMMAND_CLASS_THERMOSTAT_MODE',
+			command_get: 'THERMOSTAT_MODE_GET',
+			command_report: 'THERMOSTAT_MODE_REPORT',
+			command_report_parser: report => {
+				var state_on = false;
+				if (report && report.hasOwnProperty('Level') &&
+					report['Level'].hasOwnProperty('Mode') &&
+					typeof report['Level']['Mode'] !== 'undefined') 
+					{
+						if (report['Level']['Mode'].toLowerCase() == 'off')
+						{
+							state_on =  false;
+						}
+						if (report['Level']['Mode'].toLowerCase() == 'heat')
+						{
+							state_on =  true;
+						}
+						return state_on;
+					}
+				return state_on;
+			},
+		},
+		measure_battery: {
+			getOnWakeUp: true,
+			command_class			: 'COMMAND_CLASS_BATTERY',
+			command_get				: 'BATTERY_GET',
+			command_report			: 'BATTERY_REPORT',
+			command_report_parser		: function( report ) {
 				return report['Battery Level (Raw)'][0];
 			}
 		},		
-		'measure_temperature': {
-			'getOnWakeUp': true,
-			'command_class'				: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
-			'command_get'				: 'SENSOR_MULTILEVEL_GET',
-			'command_get_parser'		: function(){
+		measure_temperature: {
+			getOnWakeUp: true,
+			command_class				: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			command_get				: 'SENSOR_MULTILEVEL_GET',
+			command_get_parser		: function(){
 				return {
 					'Sensor Type': 'Temperature (version 1)',
 					'Properties1': {
@@ -30,36 +55,61 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 					}
 				}
 			},
-			'command_report'			: 'SENSOR_MULTILEVEL_REPORT',
-			'command_report_parser'		: function( report ){
+			command_report			: 'SENSOR_MULTILEVEL_REPORT',
+			command_report_parser		: function( report ){
 				if( report['Sensor Type'] !== 'Temperature (version 1)' )
 					return null;
 
 				return report['Sensor Value (Parsed)'];
 			}
 		},
-		'target_temperature': {
-			'getOnWakeUp': true,
-			'setOnWakeUp': true,
-			'command_class'				: 'COMMAND_CLASS_THERMOSTAT_SETPOINT',
-			'command_get'				: 'THERMOSTAT_SETPOINT_GET',
-			'command_get_parser'		: function(){
-				return {
-					'Sensor Type': 'Set Point (version 1)',
-					'Properties1': {
-						'Scale': 0
-					}
+		target_temperature: {   
+			getOnWakeUp: true,		   
+			command_class: 'COMMAND_CLASS_THERMOSTAT_SETPOINT',
+			command_get: 'THERMOSTAT_SETPOINT_GET',
+			command_get_parser: () => ({
+				'Level': {
+					'Setpoint Type': 'Heating 1',
+					 
+				
+	  
 				}
-			},
-			'command_set'				: 'THERMOSTAT_SETPOINT_SET',
-			'command_report'			: 'THERMOSTAT_SETPOINT_REPORT',
-			'command_report_parser'		: function( report ){
-				if( report['Sensor Type'] !== 'Set Point (version 1)' )
-					return null;
+			}),
+			command_set: 'THERMOSTAT_SETPOINT_SET',
+			command_set_parser: (value) => {
+												 
+														   
+				 
 
-				return report['Sensor Value (Parsed)'];
-			}
+				// Create value buffer
+				const a = new Buffer(2);
+				a.writeUInt16BE((Math.round(value * 2) / 2 * 10).toFixed(0));
+
+				return {
+					'Level': {
+						'Setpoint Type': 'Heating 1'
+					},
+					'Level2': {
+						'Size': 2,
+						'Scale': 0,
+						'Precision': 1
+					},
+					'Value': a
+				};
+			},
+			command_report: 'THERMOSTAT_SETPOINT_REPORT',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Level2')
+					&& report.Level2.hasOwnProperty('Scale')
+					&& report.Level2.hasOwnProperty('Precision')
+					&& report.Level2['Scale'] === 0
+					&& report.Level2['Size'] !== 'undefined'
+					&& typeof report['Value'].readUIntBE(0, report.Level2['Size']) !== 'undefined') {
+					return report['Value'].readUIntBE(0, report.Level2['Size']) / Math.pow(10, report.Level2['Precision']);
 				}
+				return null;
+			},
+		}
 	},
 	settings: {
 		"enable_temperature_sensor": {
